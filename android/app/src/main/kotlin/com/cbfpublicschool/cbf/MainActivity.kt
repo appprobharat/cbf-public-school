6a20d879224942004f8f62034b4c8986a20c9d9f
Vishal Raj <vishalraj27439@gnail.com>
2026-06-11 10:13:38 +0530

    chnges in main file

---
+++ android/app/src/main/kotlin/com/cbfpublicschool/cbf/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cbfpublicschool.cbf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -157,20 +157,20 @@
 // }
 
 import 'package:flutter/material.dart';
-import 'package:cbf/login_page.dart';
+
+
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
-    );
-  }
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'HELLO',
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
+      ),
+    ),
+  );
 }
